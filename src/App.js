d4cb8e308228bb6ed13297351e2e19348f04798d
Frederico Zanoni Ruiz <fredericozanoni@gmail.com>
2022-06-17 18:13:37 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
+const GIFS = [
+  `https://media3.giphy.com/media/WJjLyXCVvro2I/giphy.gif?cid=ecf05e47bd7c52dac7526223075e6ad61dc867860a792cc2&rid=giphy.gif&ct=g`,
+  `https://media1.giphy.com/media/fxfuiruxVLbdb2fiAy/giphy.webp`
+]
+const diferentGifts = [
+  `https://media0.giphy.com/media/vJ6ikzK0kL5h719fXc/giphy.webp`,
+  `https://media1.giphy.com/media/IZvPPwILs6ZGFbvLdO/giphy.webp`
+]
 function App() {
+
+  const [ value, updateValue ] = useState(GIFS)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          value.map(gif => <img src={gif} alt="gif" />)
+        }
+        <button onClick={ ()=> updateValue(diferentGifts) }>Cambiar Gifs</button>
+      </section>
     </div>
   );
 }
